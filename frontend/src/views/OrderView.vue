403288a38d3d8011b8f537eea2334d091b71a25d
Manuel Kienlein <manuel.kienlein@gmx.de>
2025-11-22 18:08:48 +0100

    Implemented ui for orders

--- frontend/src/views/OrderView.vue
+++ frontend/src/views/OrderView.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 
 import OrdersTable from "@/components/OrdersTable.vue";
+import LegacyOrdersTable from "@/components/LegacyOrdersTable.vue";
 </script>
 
 <template>
-<OrdersTable></OrdersTable>
+  <OrdersTable></OrdersTable>
+
+  <hr class="m-5">
+
+  <LegacyOrdersTable></LegacyOrdersTable>
 </template>
 
 <style scoped>
